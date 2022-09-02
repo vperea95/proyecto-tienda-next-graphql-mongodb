9a693734baad15199339966361e9ed712e03a34e
Victor pc <andreesiitoo1015@gmail.com>
2022-09-02 18:40:07 -0500

    modificacion mensajes de conexio a la BD

--- pages/api/config/db.js
+++ pages/api/config/db.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
-require('dotenv').config({ path: '.env' });
+const mongoose = require("mongoose");
+require("dotenv").config({ path: ".env" });
 
 const conectarDB = async () => {
-    try {
-        await mongoose.connect(process.env.DB_MONGO, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
-        });
-        console.log('DB Conectada');
-    } catch (error) {
-        console.log('Hubo un error');
-        console.log(error);
-        process.exit(1); // detener la app
-    }
-}
+  try {
+    await mongoose.connect(process.env.DB_MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+    console.log("DB Conectada con exito");
+  } catch (error) {
+    console.log("Hubo un error en la conexion a la BD");
+    console.log(error);
+    process.exit(1); // detener la app
+  }
+};
 
 module.exports = conectarDB;
